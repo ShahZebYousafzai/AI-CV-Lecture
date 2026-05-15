09defec65d731958f57863f17163d7652da73f4d
ShahZebYousafzai <zebshah7851@gmail.com>
2026-05-15 09:21:39 +0500

    Added infer video

---
+++ Session 10/infer_video.py
@@ -0,0 +1,36 @@
+import cv2
+
+from ultralytics import YOLO
+
+# Load the YOLO model
+model = YOLO("yolov8n.pt")
+
+# Open the video file
+video_path = "test_video.mp4"
+cap = cv2.VideoCapture(0)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLO inference on the frame
+        results = model(frame, conf=0.25)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLO Inference", cv2.resize(annotated_frame, (640, 480)))
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
